8458fea3293854f1ddb252bc9a4e6306f0d16157
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-05-10 18:18:47 -0500

    spotify-this-song ready for -one word- songs only

--- liri.js
+++ liri.js
@@ -9,35 +9,59 @@ var keys = require("./keys.js");
 // This package also optionally works with promises. Just omit the callback parameter and the search method returns a promise object containing the response: 
 var Spotify = require('node-spotify-api');
 
-// code to access keys information
-
-function spotifyThisSong() {
+// code for function when command is spotify this song
+function spotifyThisSong(songName) {
     var spotify = new Spotify(keys.spotify);
 
-    spotify
-        .search({ type: 'track', query: 'All the Small Things', limit: 1 })
-        .then(function (response) {
-            console.log("Artist: " + response.tracks.items[0].album.artists[0].name +
-                "\nSong: " + response.tracks.items[0].name +
-                "\nSong Link: " + response.tracks.items[0].external_urls.spotify +
-                "\nAlbum: " + response.tracks.items[0].album.name);
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-}
-
-function options(commandData, functionData) {
-    switch(commandData) {
-        case 'spotify-this-song' :
-        spotifyThisSong();
-        break;
-        // need to add default case later
+    // Making sure that there is a value for the query, and the code is set to show 3 results, while making tests even if the search type is track, is not always fetching the expected results
+    if (songName) {
+        spotify
+            .search({ type: 'track', query: songName, limit: 3 })
+            .then(function (response) {
+                let song = response.tracks.items;
+                for (i = 0; i < song.length; i++) {
+                    console.log("Result: " + (i + 1) +
+                        "\nArtist: " + song[i].album.artists[0].name +
+                        "\nSong: " + song[i].name +
+                        "\nSong Link: " + song[i].external_urls.spotify +
+                        "\nAlbum: " + song[i].album.name +
+                        "\n----------------------------------------------------------------------------------");
+                }
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+    // If there is no query at all, the default song to display is The Sign by Ace of Base
+    else {
+        spotify
+            .search({ type: 'track', query: "The Sign Ace of Base", limit: 1 })
+            .then(function (response) {
+                let song = response.tracks.items;
+                console.log("Artist: " + song[0].album.artists[0].name +
+                    "\nSong: " + song[0].name +
+                    "\nSong Link: " + song[0].external_urls.spotify +
+                    "\nAlbum: " + song[0].album.name);
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     }
 }
 
+// code for function to choose a specific command when using the app
+function options(commandData, functionData) {
+    switch (commandData) {
+        case 'spotify-this-song':
+            spotifyThisSong(functionData);
+            break;
+    }
+}
+
+// code for main function that runs the app
 function runApp(argOne, argTwo) {
     options(argOne, argTwo);
 }
 
+// calling main function
 runApp(process.argv[2], process.argv[3]);
