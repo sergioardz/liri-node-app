c1e6af04654beefc6a27960a2ebd0455015bbdb6
Sergio Armando Rodriguez Ramirez <sergioardz79@gmail.com>
2019-05-11 15:19:12 -0500

    concert-this functionality ready and working fine

--- liri.js
+++ liri.js
@@ -7,6 +7,8 @@ var keys = require("./keys.js");
 // code to require axios package
 var axios = require("axios");
 
+var moment = require("moment");
+
 // code to fetch the command option to execute based on arguments
 var term = process.argv[2];
 
@@ -18,24 +20,56 @@ var queryContent = process.argv.slice(3).join("+");
 // This package also optionally works with promises. Just omit the callback parameter and the search method returns a promise object containing the response: 
 var Spotify = require('node-spotify-api');
 
-// code for function when command is spotify this song
+// code for function when command is spotify-this-song
 function spotifyThisSong(songName) {
     var spotify = new Spotify(keys.spotify);
     spotify
         .search({ type: 'track', query: songName, limit: 1 })
         .then(function (response) {
             let song = response.tracks.items;
-            console.log("Artist: " + song[0].album.artists[0].name +
+            var songResults =
+                "Artist: " + song[0].album.artists[0].name +
                 "\nSong: " + song[0].name +
                 "\nSong Link: " + song[0].external_urls.spotify +
-                "\nAlbum: " + song[0].album.name);
+                "\nAlbum: " + song[0].album.name;
+            console.log(songResults);
         })
         .catch(function (err) {
             console.log(err);
         });
 }
 
-// code for function when command is movie this
+// code for function when command is concert-this
+function fetchConcert(artistName) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+    axios
+        .get(queryUrl)
+        .then(
+            function (response) {
+                info = response.data;
+                if (info.length === 0) {
+                    var noInfo = "There are no info or upcoming concerts for that artist. You can try other one."
+                    console.log(noInfo);
+                }
+                else {
+                    for (i = 0; i < info.length; i++) {
+                        var concertsResults =
+                            "Result # " + (i + 1) +
+                            "\nName of the venue: " + info[i].venue.name +
+                            "\nVenue location: " + info[i].venue.city + ", " + info[i].venue.region + ", " + info[i].venue.country +
+                            "\nDate of the event: " + moment(info[i].datetime).format('MM/DD/YYYY');
+                        console.log(concertsResults);
+                    }
+                }
+
+            }
+        )
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+// code for function when command is movie-this
 function fetchMovie(movieName) {
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     axios
@@ -43,23 +77,25 @@ function fetchMovie(movieName) {
         .then(
             function (response) {
                 var info = response.data;
-                console.log(
+                var movieResults =
                     "* Title of the movie: " + info.Title +
                     "\n* Year the movie came out: " + info.Year +
                     "\n* IMDB Rating of the movie: " + info.imdbRating +
+                    "\n* Rotten Tomatoes Rating of the movie: " + info.Ratings[1].Value +
                     "\n* Country where the movie was produced: " + info.Country +
                     "\n* Language(s) of the movie: " + info.Language +
                     "\n* Plot of the movie: " + info.Plot +
-                    "\n* Actors in the movie: " + info.Actors);
-                if (info.Ratings.length > 1) {
-                    if (info.Ratings[1].Source === "Rotten Tomatoes") {
-                        console.log(
-                            "* Rotten Tomatoes Rating of the movie: " + info.Ratings[1].Value);
-                    }
-                }
-                else {
-                    console.log("--- No Rotten Tomatoes Rating available for " + info.Title + " ---");
-                }
+                    "\n* Actors in the movie: " + info.Actors;
+                console.log(movieResults);
+                // if (info.Ratings.length > 1) {
+                //     if (info.Ratings[1].Source === "Rotten Tomatoes") {
+                //         console.log(
+                //             "* Rotten Tomatoes Rating of the movie: " + info.Ratings[1].Value);
+                //     }
+                // }
+                // else {
+                //     console.log("--- No Rotten Tomatoes Rating available for " + info.Title + " ---");
+                // }
             }
         )
         .catch(function (error) {
@@ -86,6 +122,14 @@ function options(commandData, functionData) {
             }
             else {
                 fetchMovie(functionData);
+            }
+            break;
+        case 'concert-this':
+            if (!functionData) {
+                console.log("Please input an artist name");
+            }
+            else {
+                fetchConcert(functionData);
             }
             break;
     }
